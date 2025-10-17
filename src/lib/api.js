fb76740063f7f9437be44daf5fc8cec97ffc48e9
mingyu <78838545+zydovvn@users.noreply.github.com>
2025-10-18 04:08:32 +0700

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
-const api = axios.create({
-  baseURL: API_URL,
+export const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
-export default api;
+export default API; // (tuỳ chọn) để nơi khác có thể import default
